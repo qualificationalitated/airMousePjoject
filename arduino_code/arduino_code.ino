aaa82df50fa34dea0cf5048ab19c8a308f7dc166
qualificationalitated <rlagusdn9548@gmail.com>
2019-11-29 16:12:37 +0900

    Update arduino_code.ino

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,4 +1,4 @@
-// Gun for Shooting Game 
+ // Gun for Shooting Game 
 // 2016-12-31 Yoo JaeSung - blog.naver.com/dev4unet 
 //  
 // MPU-6050 Short Example Sketch 
@@ -10,19 +10,19 @@
 const int MPU_addr=0x68; 
 // I2C address of the MPU-6050 
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ; 
-int ledPin = 4; 
+// int ledPin = 4; 
 int buttonPin = 5; 
 void setup()
 {   
-  pinMode(ledPin, OUTPUT);   
+//  pinMode(ledPin, OUTPUT);   
   pinMode(buttonPin, INPUT_PULLUP);      
-  Wire.begin();   
-  Wire.beginTransmission(MPU_addr);   
-  Wire.write(0x6B); // PWR_MGMT_1 register   
-  Wire.write(0); // set to zero (wakes up the MPU-6050)  
-  Wire.endTransmission(true);  
-  Serial.begin(115200);  
-  Mouse.begin(); 
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B); // PWR_MGMT_1 register
+  Wire.write(0); // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  Serial.begin(115200);
+  Mouse.begin();
   Serial.println("Mouse Start!!");
 }
 void loop()
@@ -30,34 +30,34 @@ void loop()
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU_addr,14,true); // request a total of 14 registers  
+  Wire.requestFrom(MPU_addr,14,true); // request a total of 14 registers
   AcX=Wire.read()<<8|Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
   AcY=Wire.read()<<8|Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  AcZ=Wire.read()<<8|Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L) 
+  AcZ=Wire.read()<<8|Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
   Tmp=Wire.read()<<8|Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  GyX=Wire.read()<<8|Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)  
-  GyY=Wire.read()<<8|Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L) 
-  GyZ=Wire.read()<<8|Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)   
-  int16_t gyroX, gyroZ;  
-  int Sensitivity = 2000; 
-  gyroX = GyX / Sensitivity / 1.1 * -1;  
-  gyroZ = GyZ / Sensitivity * -1; 
-  Mouse.move(gyroZ, gyroX);   
+  GyX=Wire.read()<<8|Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  int16_t gyroX, gyroZ;
+  int Sensitivity = 1000; // 움직임 보정치
+  gyroX = GyX / Sensitivity / 2.0 * -1; //마우스 세로 움직임 보정
+  gyroZ = GyZ / Sensitivity * -1; // 마우스 가로 움직임 보정
+  Mouse.move(gyroZ, gyroX);
   if (digitalRead(buttonPin) == LOW)
-  {     
+  {
     if (!Mouse.isPressed(MOUSE_LEFT))
-    {  
+    {
       Mouse.press(MOUSE_LEFT);
-      digitalWrite(ledPin, HIGH);
-    } 
-  }  
-  else 
-  {     
+      //digitalWrite(ledPin, HIGH);
+    }
+  }
+  else
+  {  
     if (Mouse.isPressed(MOUSE_LEFT))
     {
       Mouse.release(MOUSE_LEFT);
-      digitalWrite(ledPin, LOW);
-    } 
+      //digitalWrite(ledPin, LOW);
+    }
   }
 }
 /*
